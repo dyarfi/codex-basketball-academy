21cb45c3bb9daab640a00ce032807e73a6283e79
Defrian Yarfi <defrian.yarfi@bankneo.co.id>
2026-04-03 08:57:42 +0700

    feat: define GraphQL auth schema with mutations and queries

---
+++ api/src/graphql/auth.sdl.ts
@@ -0,0 +1,30 @@
+// api/src/graphql/auth.sdl.ts
+export const schema = gql`
+  type AuthPayload {
+    user: User!
+  }
+
+  type PasswordResetResult {
+    success: Boolean!
+    message: String
+  }
+
+  type Query {
+    me: User @requireAuth
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): AuthPayload
+    signup(
+      email: String!
+      password: String!
+      firstName: String!
+      lastName: String!
+      role: Role!
+      dateOfBirth: DateTime
+    ): AuthPayload
+    forgotPassword(email: String!): Boolean
+    resetPassword(resetToken: String!, password: String!): AuthPayload
+    logout: Boolean
+  }
+`
