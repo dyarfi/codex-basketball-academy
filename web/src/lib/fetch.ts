84fec36aade4cb8cb0e5b9cabb6ebdc50e34043b
Defrian Yarfi <defrian.yarfi@gmail.com>
2026-05-21 22:16:53 +0700

    feat: major updates and features

--- web/src/lib/fetch.ts
+++ web/src/lib/fetch.ts
@@ -6,6 +6,7 @@ const C_CLOUD = process.env.CLOUDINARY_NAME
 const C_TSTAMP = Date.now() / 1000
 const C_UP_URL = `https://api.cloudinary.com/v1_1/${C_CLOUD}/image/upload`
 const C_LC_URL = `https://api.cloudinary.com/v1_1/${C_CLOUD}/resources/search`
+const KEY_BREVO = process.env.KEY_BREVO
 
 export function uploadCloud(file: any, folder: string) {
   return new Promise((resolve, reject) => {
@@ -64,59 +65,6 @@ export function listCloud() {
         return reject(err || new Error('Network error during listing.'))
       })
   })
-
-}
-
-export function sendWhatsAppBusiness({
-  subject,
-  phone,
-  messages,
-}: {
-  subject: string
-  phone: number
-  messages: string
-}) {
-  const url = 'https://graph.facebook.com/v22.0/722635610936779/messages'
-
-  const payload = {
-    messaging_product: 'whatsapp',
-    // to: '+62' + phone,
-    to: '+6287882276763',
-    type: 'text',
-    text: {
-      // the text object
-      // preview_url: false,
-      body: 'Hello asdadsa asdasda. Site: https://dykraf.com',
-    },
-    // template: {
-    //   name: 'hello_world',
-    //   language: { code: 'en_US' },
-    // },
-  }
-  //  EAAKWlHqY26QBPS2s1GsvG3vKIadzCkt8DeYd01EBTd1TKv6FHZAWtZBpIxYyFPSDNTUjVOZB4cpOQ5WgaqGvqXq9GnaH8wiCTg5T8m3FwDwCHpHrI2NQJ7qp5N7gn9U8GaOTYy8afVhhQPQ1ExP2LpavMCfLk8Fb4kQKcZBSDUrwyW6Ta0NJQwj6cUW7hCeAZAwZDZD
-  const headers = {
-    Authorization:
-      'Bearer EAAQI5nP84UABPVR2kz2hYkjJxXZC9sGAJrFHZB5da0X2zmtPLBUvXwnSb0910x1bIEJiPZCRluPU64oggWolxiTyJ5kxunLKplYaPxaq0FV1xYeZCXX067ZCDGFlpFnhHYuZC13eJAJDbZCGIZCzopn0s9vVm61wWPQZBzcMRFpVgxYDwSc8RFEzn2xVbfZBZC3k7ZCpZCwZDZD',
-    'Content-Type': 'application/json',
-  }
-
-  fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(payload),
-  })
-    .then(async (res) => {
-      if (!res.ok) {
-        const errorData = await res.json().catch(() => ({}))
-        console.error('Error sending message:', errorData)
-        return
-      }
-      const data = await res.json()
-      console.log('Message sent:', data)
-    })
-    .catch((error) => {
-      console.error('Network or fetch error:', error)
-    })
 }
 
 export async function sendEmailMessage({
@@ -151,8 +99,7 @@ export async function sendEmailMessage({
   }
 
   const headers = {
-    'api-key':
-      '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',
+    'api-key': KEY_BREVO as string,
     accept: 'application/json',
     'Content-Type': 'application/json',
   }
